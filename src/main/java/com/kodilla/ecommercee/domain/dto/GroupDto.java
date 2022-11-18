2e0ae5aadafc0b87382ce139bfe085515f4a2931
Mariusz Pawłowski <mpawlowski15@interia.pl>
2022-11-19 00:59:14 +0100

    Initial implementation of GroupController

---
+++ src/main/java/com/kodilla/ecommercee/domain/dto/GroupDto.java
@@ -0,0 +1,14 @@
+package com.kodilla.ecommercee.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GroupDto {
+
+    private Long groupId;
+    private String groupName;
+}
