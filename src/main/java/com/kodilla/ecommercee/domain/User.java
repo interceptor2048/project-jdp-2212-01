85d72d9c597cfb5d5b1876f6a4225a33f1778c61
Mateusz Olszewski <matcrono@gmail.com>
2022-11-25 14:22:20 +0800

    Finished User Entity

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -5,29 +5,34 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
-@Getter
-@Entity(name = "USERS")
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Entity(name = "USERS")
 public class User {
+
     @Id
     @GeneratedValue
     @NotNull
     @Column(name = "USER_ID")
     private Long userId;
 
+    @Column(name = "USERNAME")
     private String username;
 
+    @Column(name = "FIRSTNAME")
     private String firstName;
 
+    @Column(name = "LASTNAME")
     private String lastName;
 
+    @Column(name = "ACCOUNT_BLOCKED")
     private Boolean isBlocked;
 
+    @Column(name = "USERKEY")
     private Long userKey;
 }
