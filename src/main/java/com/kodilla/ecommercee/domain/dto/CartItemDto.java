6aac1167a246aa4ae70bb4dfffdaf156c29e219c
Rafal Glodek <rafal.glodek@interia.pl>
2022-11-25 13:09:08 +0000

    Resolved conflict

--- src/main/java/com/kodilla/ecommercee/domain/dto/CartItemDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/CartItemDto.java
@@ -1,15 +1,17 @@
 package com.kodilla.ecommercee.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+
+import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
 public class CartItemDto {
-
     private long id;
-    private long product_id;
-    private long order_id;
+    private long productId;
     private long quantity;
-    private long unit_Price;
+    private BigDecimal unitPrice;
+    private long orderId;
 }
---
+++ src/main/java/com/kodilla/ecommercee/domain/dto/OrderDto.java
@@ -0,0 +1,19 @@
+package com.kodilla.ecommercee.domain.dto;
+
+import com.kodilla.ecommercee.domain.CartStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+public class OrderDto {
+
+    private Long id;
+    private Long userId;
+    private LocalDateTime dateTime;
+    private CartStatus cartStatus;
