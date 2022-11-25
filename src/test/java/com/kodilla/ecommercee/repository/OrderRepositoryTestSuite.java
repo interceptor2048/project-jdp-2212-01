6f2d60263376ef01ced7de9afe1780105e4e2037
Julian Zaleski <julizale@gmail.com>
2022-11-25 23:02:06 +0100

    Added tests

--- src/test/java/com/kodilla/ecommercee/repository/OrderRepositoryTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/repository/OrderRepositoryTestSuite.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class OrderRepositoryTestSuite {
@@ -34,4 +36,51 @@ public class OrderRepositoryTestSuite {
         //Cleanup
         orderRepository.deleteById(orderId);
     }
+
+    @Test
+    void testOrderRepositoryFindById() {
+        //Given
+        Order order = new Order();
+        orderRepository.save(order);
+        long orderId = order.getId();
+
+        //When
+        Optional<Order> orderFoundById = orderRepository.findById(orderId);
+
+        //Then
+        try {
+            assertTrue(orderFoundById.isPresent());
+        } finally {
+            //Cleanup
+            orderRepository.deleteById(orderId);
+        }
+    }
+
+    @Test
+    void testOrderRepositoryDeleteById() {
+        //Given
+        Order order1 = new Order();
+        Order order2 = new Order();
+        orderRepository.save(order1);
+        orderRepository.save(order2);
+        long order1Id = order1.getId();
+        long order2Id = order2.getId();
+
+        //When
+        orderRepository.deleteById(order1Id);
+        Optional<Order> order1FoundById = orderRepository.findById(order1Id);
+        Optional<Order> order2FoundById = orderRepository.findById(order2Id);
+
+        //Then
+        try {
+            assertFalse(order1FoundById.isPresent());
+            assertTrue(order2FoundById.isPresent());
+        } finally {
+            //Cleanup
+            orderRepository.deleteById(order2Id);
+        }
+
+    }
+
+
 }
