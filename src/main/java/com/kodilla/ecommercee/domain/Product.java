07b005535d1dc1bc8d52100e728a3341284931eb
Rafal Glodek <rafal.glodek@interia.pl>
2022-11-26 18:20:18 +0000

    Encja Produkt

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -4,12 +4,17 @@ import com.sun.istack.NotNull;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Setter
+@Getter
+@Table(name = "PRODUCT")
 public class Product {
 
     @Id
@@ -17,6 +22,21 @@ public class Product {
     @NotNull
     @Column(name = "PRODUCT_ID")
     private long id;
+    @Column(name="NAME")
+    private  String name;
+    @Column(name="DESCRIPTION")
+    private String description;
+    @Column(name="PRICE")
+    private int price;
+    @Column(name="GROUPID")
+    private String groupId;
+    @OneToMany(
+            targetEntity = CartItem.class,
+            mappedBy = "product",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<CartItem> cartItems =new ArrayList<>();
 
     @ManyToOne
     @JoinColumn(name = "GROUP_ID")
