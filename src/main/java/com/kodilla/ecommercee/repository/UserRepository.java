85449aca58d95039f1bb9f7177f50e3f33d5ca71
Mateusz Olszewski <matcrono@gmail.com>
2022-11-28 14:08:55 +0800

    Final branch for JDP221201-15, fixed User entity

--- src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.User;
 import org.springframework.data.repository.CrudRepository;
-<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,8 +10,4 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<User, Long> {
 
     Optional<User> findById(Long taskId);
-=======
-
-public interface UserRepository extends CrudRepository<User, Long> {
->>>>>>> c577fa7d24b92b4f44c054f13fa8565434fbc3c3
 }
