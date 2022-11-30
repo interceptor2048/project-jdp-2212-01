32efbaf411b16f847d5fe39539eeab26a7f15db1
Mateusz Olszewski <matcrono@gmail.com>
2022-12-01 00:43:36 +0800

    Initial branch and first test

---
+++ src/main/java/com/kodilla/ecommercee/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.kodilla.ecommercee.mapper;
+
+import com.kodilla.ecommercee.domain.User;
+import com.kodilla.ecommercee.domain.dto.UserDto;
+
+public class UserMapper {
+
+    public UserDto mapToUserDto(final User user) {
+        return new UserDto(
+                user.getUserId(),
+                user.getUsername(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getIsBlocked(),
+                user.getUserKey());
+    }
+
+}
